9cc7406007cc299cc28951d5c9266a765d67d5a8
DiegoCortez04 <c.diego2004@gmail.com>
2023-05-16 12:17:55 -0500

    SUBIENDO REPO

---
+++ Clases/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objetos.Clases
+{
+    //CLASE ES UNA PLANTILLA QUE CONTIENE OBJETOS Y METODOS
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public double Salario { get; set; }
+
+        public void ImprimirDatos()
+        {
+            Console.WriteLine("TU NOMBRE ES: " + this.Nombre);
+            Console.WriteLine("TU APELLIDO ES: " + this.Apellido);
+            Console.WriteLine("TU EDAD ES: " + this.Edad);
+            Console.WriteLine("TU SALARIO ES: " + this.Salario);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Objetos.Clases;
+using System;
+
+namespace Objetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            #region PERSONA 1
+            Persona persona1 = new Persona();
+            persona1.Nombre = "Maria";
+            persona1.Apellido = "Sosa";
+            persona1.Edad = 25;
+            persona1.Salario = 12313.312;
+
+            Console.WriteLine(persona1.Nombre);
+
+            persona1.ImprimirDatos();
+
+            Console.WriteLine("__________________________________");
+            #endregion
+            #region PERSONA 2
+            Persona persona2 = new Persona();
+            persona2.Nombre = "Jorge";
+            persona2.Apellido = "Peña";
+            persona2.Edad = 18;
+            persona2.Salario = 5;
+
+            persona2.ImprimirDatos();
+
+            Console.WriteLine("__________________________________");
+            #endregion
+            #region PERSONA 3
+            Persona persona3 = new Persona();
+            persona3.Nombre = "David";
+            persona3.Apellido = "Gonzalez";
+            persona3.Edad = 19;
+            persona3.Salario = 10;
+
+            persona3.ImprimirDatos();
+
+            Console.WriteLine("__________________________________");
+            #endregion
+        }
+    }
+}
